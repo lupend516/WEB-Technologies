050ea54e48dfe402dc7a6b94d1cd9480c1d0a9df
Relbi3000 <158342048+Relbi3000@users.noreply.github.com>
2024-09-12 04:34:02 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect, url_for
 import psycopg2
 
 app = Flask(__name__)
@@ -6,20 +6,13 @@ app = Flask(__name__)
 # Функция для подключения к базе данных
 def connect_db():
     conn = psycopg2.connect(
-        host="localhost",
-        database="students",
+        dbname="students",
         user="postgres",
-        password='Asan-bina2004'
-    )
-    return conn
-
-"""conn = psycopg2.connect(
-        dbname="rizeup",
-        user="postgres",
-        password="Asan-bina2004",
+        password="Asan-bina2004",  # Замените 'your_password' на ваш реальный пароль
         host="localhost",
         port="1996"
-    )"""
+    )
+    return conn
 
 # Отображение HTML формы
 @app.route('/')
@@ -29,25 +22,33 @@ def form():
 # Обработка данных формы
 @app.route('/submit', methods=['POST'])
 def submit():
-    fname = request.form['fname']
-    lname = request.form['lname']
-    group = request.form['segroup']
-    weekday = request.form['weekday']
-    section = request.form['section']
-    time = request.form['time']
-    message = request.form.get('message', '')
+    try:
+        fname = request.form['fname']
+        lname = request.form['lname']
+        group = request.form['segroup']
+        weekday = request.form['weekday']
+        section = request.form['section']
+        time = request.form['time']
+        message = request.form.get('message', '')
 
-    # Вставка данных в базу данных
-    conn = connect_db()
-    cur = conn.cursor()
-    cur.execute("""
-        INSERT INTO registrations (fname, lname, group_name, weekday, section, time_slot, message)
-        VALUES (%s, %s, %s, %s, %s, %s, %s)
-    """, (fname, lname, group, weekday, section, time, message))
-    conn.commit()
-    cur.close()
-    conn.close()
+        # Вставка данных в базу данных
+        conn = connect_db()
+        cur = conn.cursor()
+        cur.execute("""
+            INSERT INTO registrations (fname, lname, group_name, weekday, section, time_slot, message)
+            VALUES (%s, %s, %s, %s, %s, %s, %s)
+        """, (fname, lname, group, weekday, section, time, message))
+        conn.commit()
+        cur.close()
+        conn.close()
 
+        return redirect(url_for('success'))
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return "An error occurred during registration."
+
+@app.route('/success')
+def success():
     return "Registration successful!"
 
 if __name__ == '__main__':
